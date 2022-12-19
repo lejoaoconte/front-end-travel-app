89f6804ced1efad76d41b7211fc4a5bd98cf429a
Your Name <lejoaoconte@gmail.com>
2022-12-19 00:06:57 -0300

    Finish webpack config:

---
+++ webpack.dev.js
@@ -0,0 +1,57 @@
+const path = require("path");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const WorkboxPlugin = require("workbox-webpack-plugin");
+
+module.exports = {
+  entry: "./src/client/index.js",
+  mode: "development",
+  devtool: "source-map",
+  stats: "verbose",
+  output: {
+    libraryTarget: "var",
+    library: "Client",
+    path: path.resolve(__dirname, "dist"),
+  },
+  resolve: {
+    fallback: {
+      fs: false,
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: "/.js$/",
+        exclude: /node_modules/,
+        loader: "babel-loader",
+      },
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.(png|ico|svg)$/,
+        loader: "file-loader",
+        options: {
+          outputPath: "assets",
+          name: "[name].[ext]",
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./src/client/views/index.html",
+      filename: "./index.html",
+    }),
+    new CleanWebpackPlugin({
+      dry: true,
+      verbose: true,
+      cleanStaleWebpackAssets: true,
+      protectWebpackAssets: false,
+    }),
+    new MiniCssExtractPlugin({ filename: "[name].css" }),
+    new WorkboxPlugin.GenerateSW(),
+  ],
+};
