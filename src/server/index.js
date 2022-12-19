785bcc691b74224b6d349ced3dbc5264a42a01c6
Your Name <lejoaoconte@gmail.com>
2022-12-19 12:16:11 -0300

    Integrate APIs on server

--- src/server/index.js
+++ src/server/index.js
@@ -25,3 +25,47 @@ app.listen(port, function () {
 app.get("/test", function (req, res) {
   res.send({ message: "running" });
 });
+
+app.get("/getcity", (req, res) => {
+  const url = process.env.GEONAME_BASE_URL;
+  const username = process.env.GEONAME_USERNAME;
+  const city = req.body.city;
+
+  const uri = `${url}&name=${city}&username=${username}`;
+
+  axios(uri)
+    .then((result) => res.send(result.data))
+    .catch((e) => {
+      res.send("Error");
+      console.log(e);
+    });
+});
+
+app.get("/getweather", (req, res) => {
+  const url = process.env.WEATHERBIT_BASE_URL;
+  const lat = req.body.lat;
+  const lon = req.body.lon;
+  const api_key = process.env.WEATHERBIT_API_KEY;
+
+  const uri = `${url}lat=${lat}&lon=${lon}&key=${api_key}`;
+
+  axios(uri)
+    .then((result) => res.send(result.data))
+    .catch((e) => {
+      res.send("Error");
+      console.log(e);
+    });
+});
+
+app.get("/images", (req, res) => {
+  const url = process.env.PIXABAY_BASE_URL;
+  const city_name = req.body.city;
+
+  const uri = `${url}&q=${city_name}&image_type=photo`;
+  axios(uri)
+    .then((result) => res.send(result.data))
+    .catch((e) => {
+      res.send("Error");
+      console.log(e);
+    });
+});
